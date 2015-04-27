19f154fea8cb830fc7ec12b4ff407777ee7dcc47
Shane Burroughs <Shane.Burroughs@gmail.com>
2015-04-26 22:22:10 -0500

    Restructured code to follow new package naming convention.

--- src/main/java/com/dsburroughs/fantasyfootball/players/Team.java
+++
@@ -1,7 +0,0 @@
-package com.dsburroughs.fantasyfootball.players;
-
-public enum Team {
-
-	CHI, ARI, GB, NYG, DET, WAS, PIT, PHI, STL, SF, CLE, IND, DAL, OAK, NE, TEN, SD, NYJ, KC, BUF, MIN, MIA, ATL, NO, CIN, SEA, TB, JAC, CAR, BAL, HOU
-
-}
